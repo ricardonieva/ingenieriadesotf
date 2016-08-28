a2180e36b3d726ca9449efacd61ed42e403e353b
FeedBack <FeedBack@FeedBack>
2016-08-28 19:21:20 -0300

    inicial 4

--- src/Modelo/Butaca.java
+++ src/Modelo/Butaca.java
@@ -2,5 +2,30 @@ package Modelo;
 
 
 public class Butaca {
-    
+     private int numero;
+     private String estado;
+
+    public Butaca(int numero, String estado) {
+        this.numero = numero;
+        this.estado = estado;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+     
+     
+     
 }
