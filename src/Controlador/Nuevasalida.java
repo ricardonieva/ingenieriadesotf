1505b53b759690058a5b0717145bc57284debc8d
FeedBack <FeedBack@FeedBack>
2016-08-28 23:41:31 -0300

    inicial 6

---
+++ src/Controlador/Nuevasalida.java
@@ -0,0 +1,20 @@
+
+package Controlador;
+
+import Vista.vistaSalida;
+
+public class Nuevasalida {
+
+    public static void salida(){
+
+       vistaSalida SA = new vistaSalida();
+    }
+ 
+    public static void crearSalida(){
+    
+        
+        
+    }
+    
+    
+}
